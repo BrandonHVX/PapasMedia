b454ab5f90da357f838620762dd3ccc55b2e9708
Brandon <brandongines@gmail.com>
2020-10-08 19:20:40 -0400

    first commit

---
+++ src/components/ServiceTab.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+class ServiceTabExample extends Component {
+  render() {
+    /* service tab menu */
+    let serviceTabMenuData = [
+      { iconName: "flaticon-002-welding", tabMenuName: "Papafans Media" },
+
+      {
+        iconName: "flaticon-015-cart",
+        tabMenuName: "Hydralife Water Services"
+      },
+      { iconName: "flaticon-010-tank-1", tabMenuName: "Ayo & Co." }
+    ];
+
+    let serviceTabMenuDatalist = serviceTabMenuData.map((val, i) => {
+      return (
+        <Tab key={i}>
+          {" "}
+          <span className="icon">
+            <i className={val.iconName} />
+          </span>{" "}
+          <span className="text">{val.tabMenuName}</span>
+        </Tab>
+      );
+    });
+
+    /* service tab content */
+
+    let serviceTabContentData = [
+      {
+        bgUrl: "image5.jpg",
+        contentTitle: "Graphic Design",
+        contentDesc:
+          "Lorem ipsum dolor sit amet, consectet adipisicin elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        serviceLink: "service-details-left-sidebar"
+      },
+      {
+        bgUrl: "image3.jpg",
+        contentTitle: "Distributor",
+        contentDesc:
+          "Lorem ipsum dolor sit amet, consectet adipisicin elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        serviceLink: "service-details-left-sidebar"
+      },
+      {
+        bgUrl: "image2.jpg",
+        contentTitle: "Film & Photography",
+        contentDesc:
+          "Lorem ipsum dolor sit amet, consectet adipisicin elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        serviceLink: "service-details-left-sidebar"
+      }
+    ];
+
+    let serviceTabContentDatalist = serviceTabContentData.map((val, i) => {
+      return (
+        <TabPanel key={i}>
+          <div
+            className="service-tab__single-content-wrapper"
+            style={{
+              backgroundImage: `url(assets/img/service/${val.bgUrl})`
+            }}
+          >
+            <div className="service-tab__single-content">
+              <h3 className="service-tab__title">{val.contentTitle}</h3>
+              <p className="service-tab__text">{val.contentDesc}</p>
+              {/* <a
+                href={`${process.env.PUBLIC_URL}/${val.serviceLink}`}
+                className="see-more-link"
+              >
+                SEE MORE
+              </a> */}
+              <a className="see-more-link">SEE MORE</a>
+            </div>
+          </div>
+        </TabPanel>
+      );
+    });
+
+    return (
+      <div>
+        {/*====================  service tab area ====================*/}
+        <div className="service-tab-area section-space--inner--120">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="section-title-area text-center">
+                  <h2 className="section-title section-space--bottom--50">
+                    Our Services <span className="title-icon" />
+                  </h2>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                {/* service tab wrapper */}
+
+                <div className="service-tab-wrapper">
+                  <Tabs>
+                    <div className="row no-gutters">
+                      <div className="col-md-4">
+                        <TabList>
+                          <div className="service-tab__link-wrapper">
+                            {serviceTabMenuDatalist}
+                          </div>
+                        </TabList>
+                      </div>
+
+                      <div className="col-md-8">
+                        {serviceTabContentDatalist}
+                      </div>
+                    </div>
+                  </Tabs>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*====================  End of service tab area  ====================*/}
+      </div>
+    );
+  }
+}
+
+export default ServiceTabExample;
